d26c17025287743e71527deac534e78d1a913c02
Martín Capparelli <mcapparelli@itba.edu.ar>
2019-03-19 10:06:11 -0300

    First Commit

---
+++ cellIndexMethod/src/main/java/ar/edu/itba/Property.java
@@ -0,0 +1,13 @@
+package ar.edu.itba;
+
+public class Property<T> {
+    T property;
+
+    public Property(T property){
+        this.property = property;
+    }
+
+    public T getProperty() {
+        return property;
+    }
+}
